83382ba20d5815e9338fbe6e0df2973d69fa67d3
Paul <p.tustain@hotmail.co.uk>
2025-05-02 06:34:26 +0100

    spell check: belp to help

--- init.lua
+++ init.lua
@@ -388,7 +388,7 @@ require('lazy').setup({
       --  :Telescope help_tags
       --
       -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `belp_tags` options and
+      -- type in the prompt window. You'll see a list of `help_tags` options and
       -- a corresponding preview of the help.
       --
       -- Two important keymaps to use while in Telescope are:
